1aa86414ab7ea3615003243567c16f7eaf8272be
Amri Karisma <amrikarisma@live.com>
2023-02-06 14:40:51 +0700

    add varcel config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ES5
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Micro service API YT Captions')
@@ -27,3 +27,5 @@ app.get('/yt-caption', async (req, res) => {
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })
+
+module.exports = app
